03502f7b3c450d3d00df1c554a305a1c82fa1836
xuanyuanxixi <1020301980@qq.com>
2019-01-02 21:30:30 +0800

    location增加构造

--- lbs/src/main/java/wangfeixixi/lbs/LocationInfo.java
+++ lbs/src/main/java/wangfeixixi/lbs/LocationInfo.java
@@ -44,4 +44,11 @@ public class LocationInfo implements Serializable {
         key = String.valueOf(latitude) + String.valueOf(longitude);
         this.rotation = rotation;
     }
+
+    public LocationInfo(String key, double latitude, double longitude, float rotation) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+        key = key;
+        this.rotation = rotation;
+    }
 }
